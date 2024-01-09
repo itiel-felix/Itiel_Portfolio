92cbf9ce534efdd2b47655d35e5f78b6282e2148
Itiel Felix <felix.itiel@uabc.edu.mx>
2024-01-09 00:24:58 -0800

    Update links for projects

--- LandingPage/Services/ProjectRepository.cs
+++ LandingPage/Services/ProjectRepository.cs
@@ -16,14 +16,16 @@ namespace LandingPage.Services
                 {
                     title = "21 Blackjack",
                     description = "Android application of a 21 Blackjack game using Jetpack Compose in Android Studio. Compatible with Android 7.0+.",
-                    imageLocation="/images/blackjack.png"
+                    imageLocation="/images/blackjack.png",
+                    projectUrl= "https://github.com/itiel-felix/21-blackjack-kotlin-compose"
                 },
                 new Project()
                 {
                     title = "Periodic Table",
                     description = "Android application with the first 50 elements in the periodic table"+
                     " using Jetpack Compose in Android Studio. Comptaible with Android 7.0+.",
-                    imageLocation="/images/periodic.png"
+                    imageLocation="/images/periodic.png",
+                    projectUrl="https://github.com/itiel-felix/Periodic-table-Android"
 
                 },
             };
